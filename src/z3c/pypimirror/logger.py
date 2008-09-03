7ca1093259557c237ef4c2f16c799e87a7840694
andreasjung <>
2008-09-03 08:07:00 +0000

    using rotating file handler

--- src/z3c/pypimirror/logger.py
+++ src/z3c/pypimirror/logger.py
@@ -4,13 +4,14 @@ A simple logger for z3c.pypymirror
 """
 
 import logging
+import logging.handlers
 
 def getLogger(filename='/tmp/pypymirror.log', log_console=False):
 
     LOG = logging.getLogger()
     LOG.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s %(levelname)-6s %(message)s')
-    filehandler = logging.FileHandler(filename)
+    filehandler = logging.handlers.TimedRotatingFileHandler(filename, 'D', 1, backupCount=14)
     filehandler.setFormatter(formatter)
     LOG.addHandler(filehandler)
     if log_console:
